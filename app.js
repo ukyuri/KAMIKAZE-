8d5cbd22914a2c44a8dd61b270d8416750dc6353
ukyuri <46381218+ukyuri@users.noreply.github.com>
2020-07-06 17:18:23 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+// const navSlide = () => {
+//     const burger = document.querySelector('.burger');
+//     const nav = document.querySelector('.nav-links');
+//     const navLinks =document.querySelectorAll('.navlinks li');
+
+// burger.addEventListener('click', () => {
+//      //toggle Nav
+//     nav.classList.toggle('nav-active');
+
+//      //animate links
+//     navLinks.forEach((link, index) => {
+//         if (link.getElementsByClassName.animation) {
+//             link.getElementsByClassName.animation = ''
+//         } else {
+//             link.getElementsByClassName.animation = `navLinkFade 0.5s ease forward ${index / 7 + 0.3}s`;
+//         }
+
+//         });
+//     });
+
+// }
+
+// navSlide();
